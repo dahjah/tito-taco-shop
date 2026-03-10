f949aaf49ee297c834f049c8ad0fe01a22f5586a
DJ Gillespie <dj@thebloggernetwork.com>
2026-03-10 16:15:04 -0600

    chore: remove accidentally tracked folders images and out and add to gitignore

--- out/python/build/lib/openapi_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.api_api import ApiApi
-from openapi_client.api.products_api import ProductsApi
-from openapi_client.api.user_api import UserApi
-
